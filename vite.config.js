6259f05eb076c363ab158da536823d1b912ba46d
primeroluz <145052678+iilknursert@users.noreply.github.com>
2025-02-13 14:14:37 +0300

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/deneme-portfolio/', // GitHub Pages için doğru base ayarı
+  base: './', // ÖNEMLİ! Dosya yollarını düzeltir
 });
